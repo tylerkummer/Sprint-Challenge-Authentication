9ca089f3c62575fe1554e6925896dfb0a4182752
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-24 10:31:00 -0500

    Comments

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -4,12 +4,12 @@ const server = require("../api/server.js");
 const db = require("../database/dbConfig.js");
 
 describe("Auth Router", () => {
-  describe('POST to "/api/auth/register"', () => {
+  describe("POST /api/auth/register", () => {
     beforeEach(async () => {
       await db("users").truncate();
     });
 
-    it("Returns status code 201", () => {
+    it("Return 201 on success", () => {
       return request(server)
         .post("/api/auth/register")
         .send({ username: "myself", password: "pass" })
@@ -19,8 +19,8 @@ describe("Auth Router", () => {
     });
   });
 
-  describe('POST to "/api/auth/register"', () => {
-    it("Returns status code 500", () => {
+  describe("POST /api/auth/register", () => {
+    it("Return 500 on failure", () => {
       return request(server)
         .post("/api/auth/register")
         .send({ username: "myself" })
@@ -30,8 +30,8 @@ describe("Auth Router", () => {
     });
   });
 
-  describe('POST to "/api/auth/login', () => {
-    it("Returns a status code 200", () => {
+  describe("POST /api/auth/login", () => {
+    it("Return 200 on success", () => {
       return request(server)
         .post("/api/auth/login")
         .send({ username: "myself", password: "pass" })
@@ -40,7 +40,7 @@ describe("Auth Router", () => {
         });
     });
 
-    it("Returns a status code 401", () => {
+    it("Return 401 on failure", () => {
       return request(server)
         .post("/api/auth/login")
         .send({ username: "myself", password: "pppasss" })
